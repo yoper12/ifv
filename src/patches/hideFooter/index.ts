fb1f36c656d66f069cc21d800e048462f6869ce5
yoper12 <69840031+yoper12@users.noreply.github.com>
2026-04-13 20:04:24 +0200

    port hideFooter to the new api

---
+++ src/patches/hideFooter/index.ts
@@ -0,0 +1,20 @@
+import { definePatch } from "@/utils/definePatch";
+import { route } from "@/utils/route";
+
+import css from "./style.css?inline";
+
+export default definePatch({
+    css,
+    meta: {
+        description: "Hides footer.",
+        deviceTypes: ["mobile"],
+        id: "hide-footer",
+        matches: [
+            route({ host: "uczen.eduvulcan.pl" }),
+            route({ host: "dziennik-uczen.vulcan.net.pl" }),
+            route({ host: "wiadomosci.eduvulcan.pl" }),
+            route({ host: "dziennik-wiadomosci.vulcan.net.pl" }),
+        ],
+        name: "Hide footer",
+    },
+});
