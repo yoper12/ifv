f90b244479b3f426f7d10312d2017983119c261c
yoper12 <69840031+yoper12@users.noreply.github.com>
2026-04-13 18:45:19 +0200

    port hideWeekends to the new api

---
+++ src/patches/hideWeekends/index.ts
@@ -0,0 +1,19 @@
+import { definePatch } from "@/utils/definePatch";
+import { route } from "@/utils/route";
+
+import css from "./style.css?inline";
+
+export default definePatch({
+    css,
+    meta: {
+        description:
+            "Hides weekends in monthly calendars in timetable, exams and frequency views.",
+        id: "hide-weekends",
+        matches: [
+            route("*/planZajec"),
+            route("*/sprawdzianyZadaniaDomowe"),
+            route("*/frekwencja"),
+        ],
+        name: "Hide weekends in monthly calendars",
+    },
+});
