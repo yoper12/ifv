fc60b6e0ae9a4ee94988acefdf7a6229f98cd548
yoper12 <69840031+yoper12@users.noreply.github.com>
2026-04-13 21:06:53 +0200

    port arrows to the new api

---
+++ src/patches/arrows/index.ts
@@ -0,0 +1,16 @@
+import { definePatch } from "@/utils/definePatch";
+import { route } from "@/utils/route";
+
+import css from "./style.css?inline";
+
+export default definePatch({
+    css,
+    meta: {
+        description:
+            "Replaces close buttons with back buttons to indicate more accurately what action it corresponds to.",
+        deviceTypes: ["mobile"],
+        id: "arrows",
+        matches: [route("*")],
+        name: "Change close button icon",
+    },
+});
